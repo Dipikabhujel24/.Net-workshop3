95f35e909a172289b0f8f2440d23b314afb2fe07
Deepika Bhujel <dipikab062@gmail.com>
2025-11-12 12:08:32 +0545

    First

---
+++ Task2/NullCheckDemo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class NullOperations
+    {
+            public void PerformNullChecks()
+            {
+                string username = null;
+
+                // 1. Using Ternary Operator
+                string resultTernary = (username != null) ? username : "Username is not available";
+                Console.WriteLine("Ternary Operator: " + resultTernary);
+
+                // 2. Using Null-Coalescing Operator
+                string resultCoalescing = username ?? "Username is not available";
+                Console.WriteLine("Null-Coalescing Operator: " + resultCoalescing);
+
+                // 3. Using Null-Coalescing Assignment Operator
+                username ??= "DefaultUser";
+                Console.WriteLine("After Null-Coalescing Assignment: " + username);
+            }
+        }
+    }
