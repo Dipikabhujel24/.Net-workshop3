a1d8392806698bcd925bcab7007cf8ec0c021733
Deepika Bhujel <dipikab062@gmail.com>
2025-11-12 15:10:31 +0545

    First

---
+++ Task4/DayOfWeekSwitch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class DayOfWeekSwitch
+    {
+        public void ShowDay()
+        {
+            Console.Write("Enter a number between 1 and 7: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid number. Please enter between 1 and 7.");
+                    break;
+            }
+        }
+    }
+}
+
---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           DayOfWeekSwitch day = new DayOfWeekSwitch();
+            day.ShowDay();
+            Console.ReadLine();
+        }
+    }
+}
