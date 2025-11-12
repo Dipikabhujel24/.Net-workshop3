e279379a15967ac8186e5aa2acd183a6ccd7b54c
Deepika Bhujel <dipikab062@gmail.com>
2025-11-12 15:21:49 +0545

    First

---
+++ Task6/ExceptionHandling.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class ExceptionHandling
+    {
+         public void ShowExceptions()
+        {
+            // Try-Catch-Finally
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("You entered: " + num);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number format.");
+            }
+            finally
+            {
+                Console.WriteLine("Program Executed.");
+            }
+
+            // Throw example
+            Console.Write("\nEnter a password: ");
+            string password = Console.ReadLine();
+
+            try
+            {
+                if (password.Length < 6)
+                {
+                    throw new Exception("Password must be at least 6 characters.");
+                }
+                Console.WriteLine("Password accepted.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExceptionHandling exceptionDemo = new ExceptionHandling();
+            exceptionDemo.ShowExceptions();
+
+            Console.WriteLine("");
+        }
+    }
+}
