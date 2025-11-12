46aaa509c36e53256731401316610d7a36a6e1b2
Deepika Bhujel <dipikab062@gmail.com>
2025-11-12 11:56:36 +0545

    First

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create an instance of Operators class
+            Operators op = new Operators();
+
+            // Call all methods
+            op.Add(10, 5);
+            op.Subtract(10, 5);
+            op.Multiply(10, 5);
+            op.Divide(10, 5);
+            op.Divide(10, 0); // To test division by zero
+            op.OddEvenFinder(7);
+            op.OddEvenFinder(8);
         }
     }
 }
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,34 +2,9 @@
 {
     internal class Program
     {
-        public void Add(int a, int b)
+        static void Main(string[] args)
         {
-            Console.WriteLine("sum:" + (a + b));
+            Console.WriteLine("");
         }
-        public void Substract(int a, int b)
-        {
-            Console.WriteLine("difference:" + (a - b));
-        }
-        public void Multiply(int a, int b)
-        {
-            Console.WriteLine("product:" + (a * b));
-        }
-        public void Divide(int a, int b)
-        {
-            if (b != 0)
-            {
-                Console.WriteLine("Division: " + (a / b));
-            }
-            else
-            {
-                Console.WriteLine("Cannot divide by zero!");
-            }
-        }
-        public void OddEvenFinder(int number)
-        {
-            string result = (number % 2 == 0) ? "Even Number" : "Odd Number";
-            Console.WriteLine(result);
-        }
-
-    }
+     }
 }
